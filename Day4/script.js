254fda42382ceeb8fce909ddc0957ecadf888688
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-12-11 05:31:51 +0000

    Day 4

---
+++ Day4/script.js
@@ -0,0 +1,9 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.toTimeString());
